8c8c42fbc028b32d9937a17069ffb3eae0a6fe21
SokolovNetology <sokolov180375@mail.ru>
2023-07-21 16:31:18 +0300

    ДЗ "Работяга,дубль2"

--- src/worker/Main.java
+++ src/worker/Main.java
@@ -1,9 +1,11 @@
 package worker;
 
 public class Main {
+
     public static void main(String[] args) {
         OnTaskDoneListener listener = System.out::println;
-        Worker worker = new Worker(listener);
+        OnTaskErrorListener listenerError = System.out::println;
+        Worker worker = new Worker(listener,listenerError);
         worker.start();
         worker.stop();
     }
--- src/worker/Worker.java
+++ src/worker/Worker.java
@@ -1,18 +1,29 @@
 package worker;
 
-public class Worker {
+public class Worker implements OnTaskDoneListener,OnTaskErrorListener{
     private OnTaskDoneListener callback;
     private OnTaskErrorListener errorCallback;
-    public Worker(OnTaskDoneListener callback) {
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
         public void start() {
             for (int i = 0; i < 100; i++) {
                 callback.onDone("Task " + i + " is done");
             }
-            Worker(OnTaskErrorListener errorCallback){
-                public void stop() {
-                    for (int i = 0; i < 100; i++) {
-                        errorCallback.onError("Task " + i + " error");
-        }
+
+                }
+
+    @Override
+    public void onDone(String result) {
+        
+    }
+
+    @Override
+    public void onError(String error) {
+
+    }
+
+    public void stop() {
     }
 }
