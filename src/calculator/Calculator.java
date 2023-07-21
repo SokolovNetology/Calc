f45fc1750bdeabd835bdc6856842e4286ac22fcc
SokolovNetology <sokolov180375@mail.ru>
2023-07-21 12:22:16 +0300

    ДЗ "Калькулятор"

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -2,16 +2,31 @@ package calculator;
 
 import java.util.function.*;
 
-public class Calculator {
+public class Calculator  {
+
     static Supplier<Calculator> instance = Calculator::new;
+
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
-    UnaryOperator<Integer> pow = x -> x * x;
-    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y != 0) {
+            return x / y;
+        } else {
+            System.out.println("Вы пытаетесь поделить на ноль,а этого делать нельзя!");
+        }
+        return 0;
+    };
 
-    Predicate<Integer> isPositive = x -> x > 0;
-    Consumer<Integer> println = System.out::println;
+        UnaryOperator<Integer> pow = x -> x * x;
+        UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+        Predicate<Integer> isPositive = x -> x > 0;
+        //Predicate<Integer> getIsPositive = y -> y > 5;
+        Consumer<Integer> println = System.out::println;
+
+    }
 
 
-}
+
+
+
