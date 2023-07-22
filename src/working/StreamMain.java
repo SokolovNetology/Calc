4fa132645d612083b4d9f6d58c6b100647038d41
SokolovNetology <sokolov180375@mail.ru>
2023-07-22 11:11:41 +0300

    ДЗ "Работа с числами"

---
+++ src/working/StreamMain.java
@@ -0,0 +1,21 @@
+package working;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+public class StreamMain {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Stream<Integer> stream = intList.stream();
+        stream.filter(x -> x > 0);
+        stream.filter(x -> x % 2 == 0);
+        stream.sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
+
+    }
+
+}
+
+
+
+
