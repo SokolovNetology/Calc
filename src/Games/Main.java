0a97c7b83348abb6d1d5639265fb7dec1cbbbed6
SokolovNetology <sokolov180375@mail.ru>
2023-07-24 23:39:21 +0300

    ДЗ "Установка1"

--- src/Games/Main.java
+++ src/Games/Main.java
@@ -5,38 +5,42 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Создание директорий
-        File dir = new File("С://Games");
+        StringBuilder stringBuilder = new StringBuilder();
+        File dir = new File("C:Games");
         if (dir.mkdir()) {
             System.out.println("Каталог создан");
         }
-
+            stringBuilder.append("C:Games");
 
         File dir1 = new File(dir, "src");
         dir1.mkdir();
-
+            stringBuilder.append("src");
         File dir2 = new File(dir, "res");
         dir2.mkdir();
-
+             stringBuilder.append("res");
         File dir3 = new File(dir, "savegames");
         dir3.mkdir();
-
-        File dir4 = new File(dir, "temp");
+             stringBuilder.append("res");
+        File dir4 = new File(dir, "savegames");
         dir4.mkdir();
-        File mainFile = new File(dir1, "Main.java");
-        File utilsFile = new File(dir1, "Utils.java");
+             stringBuilder.append("savegames");
+        File mainFile = new File(dir1, "/папка/Main.java");
+             stringBuilder.append("/папка/Main.java");
+        File utilsFile = new File(dir1, "/папка/Utils.java");
+             stringBuilder.append("/папка/Utils.java");
         try {
             mainFile.createNewFile();
             utilsFile.createNewFile();
 
             File drawablesDir = new File(dir2, "drawables");
             drawablesDir.mkdir();
-
+               stringBuilder.append("drawables");
             File vectorsDir = new File(dir2, "vectors");
             vectorsDir.mkdir();
-
+                stringBuilder.append("vectors");
             File iconsDir = new File(dir2, "icons");
             iconsDir.mkdir();
-
+                  stringBuilder.append("icons");
             File tempFile = new File(dir2, "temp.txt");
             tempFile.createNewFile();
 
