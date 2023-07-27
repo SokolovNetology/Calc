2668bdd196227ba9c6c09df2d13a5836bc451073
SokolovNetology <sokolov180375@mail.ru>
2023-07-27 13:26:48 +0300

    ДЗ "Установка1"

--- src/Games/Main.java
+++ src/Games/Main.java
@@ -1,135 +1,61 @@
 package Games;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        StringBuilder stringBuilder = new StringBuilder();
-        File dir = new File("C:Games");
-        if (!dir.exists()) {
-            if (dir.mkdir()) {
-                System.out.println("Директория Games успешно создана");
-            } else {
-                System.out.println("Неудалось создать директорию C:Games");
-            }
-        } else {
-            System.out.println("Так директория уже есть C:Games");
+    public static void main(String[] args) throws IOException {
+        StringBuilder logBuilder = new StringBuilder();
+           List <String> directories = new ArrayList<>();
+                directories.add( "C:\\User\\sokol\\Main\\src\\Games\\");
+                directories.add( "C:\\User\\sokol\\Main\\src\\Games\\src");
+                directories.add( "C:\\User\\sokol\\Main\\src\\Games\\src\\main");
+                directories.add( "C:\\User\\sokol\\Main\\src\\Games\\src\\test");
+                directories.add( "C:\\User\\sokol\\Main\\src\\Games\\res");
+                directories.add( "C:\\User\\sokol\\Main\\src\\Games\\res\\drawables");
+                directories.add( "C:\\User\\sokol\\Main\\src\\Games\\res\\vectors");
+                directories.add( "C:\\User\\sokol\\Main\\src\\Games\\res\\icons");
+                directories.add( "C:\\User\\sokol\\Main\\src\\Games\\res\\savegames");
+                directories.add( "C:\\User\\sokol\\Main\\src\\Games\\res\\temp");
+        for (String directory : directories) {
+            createDirectory(directory, logBuilder);}
+            List<String> filePaths = new ArrayList<>();
+            filePaths.add( "C:\\User\\sokol\\Main\\src\\Games\\src\\main\\Main.java");
+            filePaths.add( "C:\\User\\sokol\\Main\\src\\Games\\src\\main\\Utils.java");
+            filePaths.add( "C:\\User\\sokol\\Main\\src\\Games\\res\\temp\\temp.txt");
+        for (String file : filePaths){
+            createFile(file,logBuilder);}
         }
-        stringBuilder.append("C:Games");
+    public static void createDirectory (String path, StringBuilder logBuilder) {
+        File directory = new File(path);
+        boolean created = directory.mkdirs();
+        String status = created ? "успешно" : "не удалось";
+        logBuilder.append("Папка ").append(path).append(" создана (дата/время) - ")
+                .append(LocalDateTime.now()).append(", ").append(status).append("\n");}
 
-        File dir1 = new File("Games/src");
+    public static void createFile(String path,StringBuilder logBuilder) throws IOException {
+        File file = new File(path);
+        try {
+            boolean created = file.createNewFile();
+            String status = created ? "успешно" : "не удалось";
 
-        if (!dir1.exists()) {
-            if (dir1.mkdir()) {
-                System.out.println("Директория Games/src успешно создана");
-            } else {
-                System.out.println("Неудалось создать директорию Games/src");
-            }
-        } else {
-            System.out.println("Так директория уже есть Games/src");
+        } catch (IOException e){
+            logBuilder.append("Ошибка при создании файла").append(path).append("дата/время  ").
+                    append(LocalDateTime.now()).append(", ").append(e.getMessage()).append("\n");
         }
-        stringBuilder.append("Games/src");
-        File dir2 = new File("Games/res");
-        if (!dir2.exists()) {
-            if (dir2.mkdir()) {
-                System.out.println("Директория Games/res успешно создана");
-            } else {
-                System.out.println("Неудалось создать директорию Games/res");
-            }
-        } else {
-            System.out.println("Так директория уже есть Games/res");
-        }
-        stringBuilder.append("Games/res");
-        File dir3 = new File("Games/savegames");
-        if (!dir3.exists()) {
-            if (dir3.mkdir()) {
-                System.out.println("Директория Games/savegames успешно создана");
-            } else {
-                System.out.println("Неудалось создать директорию Games/savegames");
-            }
-        } else {
-            System.out.println("Так директория уже есть Games/savegames");
-        }
-        stringBuilder.append("Games/save games");
-
-        File dir4 = new File("Games/temp");
-        if (!dir4.exists()) {
-            if (dir4.mkdir()) {
-                System.out.println("Директория Games/temp успешно создана");
-            } else {
-                System.out.println("Неудалось создать директорию Games/temp");
-            }
-        } else {
-            System.out.println("Так директория уже есть Games/temp");
-        }
-        stringBuilder.append("Games/temp");
-        File dir5 = new File("Games/src/main");
-        if (!dir5.exists()) {
-            if (dir5.mkdir()) {
-                System.out.println("Директория Games успешно создана Games/src/main");
-            } else {
-                System.out.println("Неудалось создать директорию Games/src/main");
-            }
-        } else {
-            System.out.println("Так директория уже есть Games/src/main");
-        }
-        stringBuilder.append("Games/src/main");
-        File dir6 = new File("Games/src/test");
-        if (!dir6.exists()) {
-            if (dir6.mkdir()) {
-                System.out.println("Директория Games успешно создана Games/src/test");
-            } else {
-                System.out.println("Неудалось создать директорию Games/src/test");
-            }
-        } else {
-            System.out.println("Так директория уже есть Games/src/test");
-        }
-        stringBuilder.append("Games/src/test");
-
-            File mainFile1 = new File("Games/src/main/Main.java");
-            File mainFile2 = new File("Games/src/main/Utils.java");
-            try {
-                if (mainFile1.createNewFile() && mainFile2.createNewFile()) {
-                    System.out.println("Файлы Main.java и Utils.java успешно созданы");
-                } else {
-                    System.out.println("Не удалось создать файлы Main.java и Utils.java");
-                }
-            } catch (Exception ev) {
-                System.out.println("Не удалось создать файлы Main.java и Utils.java");
-                ev.printStackTrace();
-            }
-            // Создание трех директорий в директории res
-            File drawablesDir = new File("Games/res/drawables");
-            File vectorsDir = new File("Games/res/vectors");
-            File iconsDir = new File("Games/res/icons");
-            if (!drawablesDir.exists() && !vectorsDir.exists() && iconsDir.exists()) {
-                if (drawablesDir.mkdir() && vectorsDir.mkdir() && iconsDir.mkdir()) {
-                    System.out.println("Директории drawables, vectors, icons успешно созданы");
-                } else {
-                    System.out.println("Не удалось создать директории drawables, vectors, icons");} }
-                else {
-                    System.out.println("Такие директории drawables, vectors, icons существуют");
-                }
-            File tempFile = new File("Games/temp/temp.txt");
-            try {
-                if (tempFile.createNewFile()) {
-                    System.out.println("Данный файл Games/temp/temp.txt успешно создан");
-                } else {
-                    System.out.println("Данный файл Games/temp/temp.txt не создан");
-                }
-            } catch (Exception e) {
-                System.out.println("Не удалось создать файл Games/temp/temp.txt");
-                e.printStackTrace();
-
-                try (FileWriter writer = new FileWriter(dir6 + "/temp.txt", false)) {
-                    writer.write(stringBuilder.toString());
-                    writer.flush();
-                } catch (IOException exception) {
-                    System.out.println(exception.getMessage());
-                }
-
+        public static void createFileLog(String filePath, String content) {
+            try (FileWriter writer = new FileWriter(filePath)) {
+                writer.write("C:\\User\\sokol\\Main\\src\\Games\\res\\temp\\text");
+                System.out.println("Лог успешно записан в файл temp.txt");
+            } catch (IOException e) {
+                System.out.println("Ошибка при записи лога в файл temp.txt: " + e.getMessage());
             }
         }
-
 
     }
+}
