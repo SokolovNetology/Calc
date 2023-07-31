5ed61e2d498993c2f15ee9f60244f72af4208bc3
SokolovNetology <sokolov180375@mail.ru>
2023-07-31 19:38:00 +0300

    ДЗ "Установка2"

--- src/Games/Main.java
+++ src/Games/Main.java
@@ -8,39 +8,42 @@ import java.util.List;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
 
         StringBuilder logBuilder = new StringBuilder();
 
         List<String> directories = List.of(
                 "C:/User/sokol/Main/src/Games/",
-                         "C:/User/sokol/Main/src/Games/src",
-                         "C:/User/sokol/Main/src/Games/src/main",
-                         "C:/User/sokol/Main/src/Games/src/test",
-                         "C:/User/sokol/Main/src/Games/res",
-                         "C:/User/sokol/Main/src/Games/res/drawables",
-                         "C:/User/sokol/Main/src/Games/res/vectors",
-                         "C:/User/sokol/Main/src/Games/res/icons",
-                         "C:/User/sokol/Main/src/Games/res/savegames",
-                         "C:/User/sokol/Main/src/Games/res/temp");
+                "C:/User/sokol/Main/src/Games/src",
+                "C:/User/sokol/Main/src/Games/src/main",
+                "C:/User/sokol/Main/src/Games/src/test",
+                "C:/User/sokol/Main/src/Games/res",
+                "C:/User/sokol/Main/src/Games/res/drawables",
+                "C:/User/sokol/Main/src/Games/res/vectors",
+                "C:/User/sokol/Main/src/Games/res/icons",
+                "C:/User/sokol/Main/src/Games/res/savegames",
+                "C:/User/sokol/Main/src/Games/res/temp");
         for (String directory : directories) {
             createDirectory(directory, logBuilder);
             //System.out.println(directory);
         }
         List<String> file = List.of(
-                          "C:/User/sokol/Main/src/Games/src/main/Main.java",
-                          "C:/User/sokol/Main/src/Games/src/main/Utils.java",
-                          "C:/User/sokol/Main/src/Games/res/temp/temp.txt");
+                "C:/User/sokol/Main/src/Games/src/main/Main.java",
+                "C:/User/sokol/Main/src/Games/src/main/Utils.java",
+                "C:/User/sokol/Main/src/Games/res/temp/temp.txt");
         for (String fileP : file) {
             createFile(fileP, logBuilder);
 
         }
+        String filePaths = "C:/User/sokol/Main/src/Games/res/temp/text";
+        String content = "/temp.txt:/";
+            createFileLog(filePaths,content);
     }
 
-    public static void createDirectory (String path, StringBuilder logBuilder) {
+    public static void createDirectory(String path, StringBuilder logBuilder) {
         File directory = new File(path);
         boolean created = directory.mkdirs();
-
         String status = created ? "успешно" : "не удалось";
         logBuilder.append("Папка ").append(path).append(" создана (дата/время) - ")
                 .append(LocalDateTime.now()).append(", ").append(status).append("\n");
@@ -49,26 +52,24 @@ public class Main {
 
     public static void createFile(String path, StringBuilder logBuilder) {
 
-         File file = new File(path);
         try {
+            File file = new File(path);
             boolean created = file.createNewFile();
             String status = created ? "успешно" : "не удалось";
-
         } catch (IOException e) {
             logBuilder.append("Ошибка при создании файла").append(path).append("дата/время  ").
                     append(LocalDateTime.now()).append(", ").append(e.getMessage()).append("\n");
         }
-              String filePaths = "C:/User/sokol/Main/src/Games/res/temp/text";
-              String content = "/temp.txt:/";
-        public static void createFileLog (String filePaths, String content) {
-
+    }
+        public static void createFileLog (String filePaths, String content){
 
             try (FileWriter writer = new FileWriter(filePaths)) {
                 writer.write(content);
+                writer.flush();
                 System.out.println(content);
             } catch (IOException e) {
                 System.out.println("Ошибка при записи лога в файл temp.txt: " + e.getMessage());
             }
         }
-        }
     }
+
